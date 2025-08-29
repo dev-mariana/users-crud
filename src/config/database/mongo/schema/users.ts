771249acb2d0d4cbf2bce5e9ebfe934f78577cc0
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2025-08-29 15:24:22 -0300

    fix: change timestamps to correct way

--- src/config/database/mongo/schema/users.ts
+++ src/config/database/mongo/schema/users.ts
@@ -1,27 +1,27 @@
 import { model, Schema } from "mongoose";
 import type { User } from "~/entities/user";
 
-export const usersSchema = new Schema<User>({
-  name: {
-    type: String,
-    required: true,
+export const usersSchema = new Schema<User>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  created_at: {
-    type: Date,
-    default: Date.now,
-  },
-  updated_at: {
-    type: Date,
-    default: null,
-  },
-});
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
 
 export const UserModel = model<User>("User", usersSchema);
